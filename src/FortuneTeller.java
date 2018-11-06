6566bb06b5f8e092d12ecc0ddbc0100c3371f229
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-11-05 17:46:41 -0800

    finished fortune teller, doggo.jpg is a funny dog picture, you'll under stand when you see it.

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -31,12 +31,12 @@ public class FortuneTeller extends JPanel implements Runnable, MouseListener {
    	 fortuneTellerImage = ImageIO.read(getClass().getResource("fortune teller.png"));
    	 // 2. Adjust the frameWidth and frameHeight variables to fit your image nicely (doesn’t need a new line of code)
    	 // 4. add a mouse listener to the frame
-   	 frame.addMouseListener(l);
+   	 frame.addMouseListener(this);
     }
 
     static void begin() {
    	 // 3. Welcome the user. Give them a hint for the secret location.
-    	JOptionPane.showMessageDialog(null, "welcome, ready to have your fortune told?");
+    	JOptionPane.showMessageDialog(null, "welcome, ready to have your fortune told? hint: chain of numbers staritng with 1");
     }
 
     @Override
@@ -44,34 +44,51 @@ public class FortuneTeller extends JPanel implements Runnable, MouseListener {
    	 int mouseX = e.getX();
    	 int mouseY = e.getY();
    	 // 5. Print the mouseX variable
-   	 
+   	 System.out.println(mouseX+"\t"+mouseY);
    	 // 6. Add the mouseY variable to the previous line so that it prints out too (no new line)
    	 // 7. Adjust your secret location co-ordinates here:
-   	 int secretLocationX = 0;
-   	 int secretLocationY = 0;
+   	 int secretLocationX = 123;
+   	 int secretLocationY = 456;
    	 /** If the mouse co-ordinates and secret location are close, we'll let them ask a question. */
    	 if (areClose(mouseX, secretLocationX) && areClose(mouseY, secretLocationY)) {
    		 // 8. Get the user to enter a question for the fortune teller
-
+JOptionPane.showInputDialog(" ask me, the all seeing dog, anything");
    		 // 9. Find a spooky sound and put it in your default package (freesound.org)
-   		 // AudioClip sound = JApplet.newAudioClip(getClass().getResource("creepy-noise.wav"));
+   		 AudioClip sound = JApplet.newAudioClip(getClass().getResource("creepy-noise.wav"));
    		 // 10. Play the sound
-
+   		 sound.play();
    		 // 11. Use the pause() method below to wait until your music has finished
-
+pause(6);
    		 // 12. Insert your completed Magic 8 ball recipe (http://bit.ly/Zdrf6d) here
-
+	Random number=new Random();
+	int num=number.nextInt(6);
+	if (num==0) {JOptionPane.showMessageDialog(null, "woof woof, yes, i smell bacon, you got any bacon?");
+	}
+	if (num==1) {JOptionPane.showMessageDialog(null, "woof woof no, dea with it, i want a dog treat");
+	}
+	if (num==2) {JOptionPane.showMessageDialog(null, "woof woof try agian later, im to hungry to think");
+	}
+	if (num==3) {JOptionPane.showMessageDialog(null, "woof woof  want bacon, i won't tell you anything until I get bacon");
+	}
+	if (num==4) {JOptionPane.showMessageDialog(null, "woof woof I don't know, i'm just a dog after all");
+	}
+	if (num==5) {JOptionPane.showMessageDialog(null, "woof woof buger, cheese buger, bacon, bacon cheese buger with onion rings, mmmmmmmmmmmmmmmmmm");
+	}
    	 }
-
     }
 
-    private boolean areClose(int mouseX, int secretLocationX) {
+    private void nextInt(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private boolean areClose(int mouseX, int secretLocationX) {
    	 return mouseX < secretLocationX + 15 && mouseX > secretLocationX - 15;
     }
 
     private void pause(int seconds) {
    	 try {
-   		 Thread.sleep(1000 * seconds);
+Thread.sleep(1000 * seconds);
    	 } catch (InterruptedException e) {
    		 e.printStackTrace();
    	 }
@@ -94,6 +111,7 @@ public class FortuneTeller extends JPanel implements Runnable, MouseListener {
    	 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
    	 frame.setResizable(false);
    	 frame.setVisible(true);
+   	showAnotherImage("doggo.jpg");
     }
 
 private void showAnotherImage(String imageName) {
