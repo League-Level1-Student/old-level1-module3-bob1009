07635d67e1aa8ae0a0c07328aef6a104447b15f5
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-11-26 17:45:02 -0800

    finshed book of illusions and started on jepordy

--- src/BookOfIllusions.java
+++ src/BookOfIllusions.java
@@ -13,7 +13,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
  
-/** We’re going to make a slideshow of cool optical illusions. When the user clicks on an illusion, a new one will be loaded. **/
+/** We’re going to make a  of cool optical illusions. When the user clicks on an illusion, a new one will be loaded. **/
 
 public class BookOfIllusions extends MouseAdapter {
 
@@ -29,26 +29,45 @@ public class BookOfIllusions extends MouseAdapter {
 
 	// 1. Make a JFrame variable and initialize it using "new JFrame()"
 	JFrame frame=new JFrame();
-	
+	JLabel label;
 	private void createBook() {
 		// 2. make the frame visible
 		frame.setVisible(true);
 		// 3. set the size of the frame
-		
+		frame.setSize(600, 600);
 		// 4. find 2 images and save them to your project’s default package
 		// 5. make a variable to hold the location of your image. e.g. "illusion.jpg"
+		String picture1="coolpic1.jpeg";
 		// 6. create a variable of type "JLabel" but don’t initialize it yet
+		
 		// 7. use the "loadImage..." methods below to initialize your JLabel
+		label=loadImageFromComputer(picture1);
 		// 8. add your JLabel to the frame
+		frame.add(label);
 		// 9. call the pack() method on the frame
-		// 10. add a mouse listener to your frame (hint: use *this*)
+		frame.pack();
+		// 10.add a mouse listener to your frame (hint: use *this*)
+		frame.addMouseListener(this);
+		String picture2="coolpic2.jpeg";
+		JLabel label2;
+		label2=loadImageFromComputer(picture2);
+	
 	}
 
 	public void mousePressed(MouseEvent e) {
+		String picture2="coolpic2.jpeg";
+		JLabel label2;
+		label2=loadImageFromComputer(picture2);
 		// 11. Print "clicked!" to the console when the mouse is pressed
+		System.out.println("clicked!!!");
 		// 12. remove everything from the frame that was added earlier
+		frame.remove(label);
 		// 13. load a new image like before (this is more than one line of code)
+		frame.add(label2);
 		// 14. pack the frame
+		frame.setSize(600, 600);
+		frame.pack();
+		
 	}
 
 	// [OPTIONAL] 15. goad your users with some annoying or witty pop-ups
